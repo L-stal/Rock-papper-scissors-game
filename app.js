5d7dac941ec6007ed3189bca666096a7d7363f3c
L-stal <stalenhag.leo@gmail.com>
2022-11-13 12:37:45 +0100

    Final
    Fixed bug on else if , simple style.

--- app.js
+++ app.js
@@ -8,48 +8,67 @@ let result;
 
 
 //for each click dispaly player choice
-choices.forEach(choice => choice.addEventListener('click', (e) =>{
+choices.forEach(pChoice => pChoice.addEventListener('click', (e) =>{
     choice = e.target.id
-    playerChoice.innerHTML = choice
-    console.log(choice)
-    genCompMove()
-    getResult()
+    playerChoice.innerHTML = choice;
+    genCompMove();
+    getResult();
 }));
 
 
 //random number computer move
 function genCompMove(){
-    const rndNmbr = Math.floor(Math.random() * 3)
-    console.log(rndNmbr)
+    const rndNmbr = Math.floor(Math.random() * 3) 
     if (rndNmbr === 0){
-        compMove = 'Rock'
+        compMove = "Rock";
     }
     if (rndNmbr === 1){
-        compMove = 'Papper'
+        compMove = "Paper";
     }
     if (rndNmbr === 2){
-        compMove = 'Scissors'
+        compMove = "Scissors";
     }
-    computerChoice.innerHTML = compMove
-}
+    console.log(compMove)
+    computerChoice.innerHTML =compMove
+};
 //Compare Resualt
 function getResult(){
-    if (compMove === playerChoice){
-        result = 'Draw';
-    }
-    else if (compMove === 'Rock' || playerChoice === 'Papper'){
-        result = 'You lose...';
-    }
-    else if (compMove === 'Papper' || playerChoice === 'Scissors'){
+    if (compMove === 'Rock' && choice === "Paper"){
         result = 'You win!';
     }
-    else if (compMove === 'Scissors' || playerChoice === 'Rock'){
+    if (compMove === 'Paper' && choice === "Scissors"){
         result = 'You win!';
     }
-    else if (compMove === 'Rock' || playerChoice === 'Papper'){
+    if (compMove === 'Scissors' && choice === "Rock"){
+        result = 'You win!';
+    }
+    if (compMove === 'Paper' && choice === "Rock"){
         result = 'You lose...';
-    }    
-    else {result = 'You lose...'}
+    } 
+     if (compMove === 'Scissors' && choice === "Paper"){
+        result = 'You lose...';
+    } 
+    if (compMove === 'Rock' && choice === "Scissors") {
+        result = 'You lose...';
+    } 
+    if (compMove === choice){
+        result = 'Its a Draw';
+    }
+    console.log(choice)
+    
+    resultsDisplay.innerHTML = result;
     console.log(result)
-    resultsDisplay.innerHTML = result
+    
 };
+/* function getResult() {
+    result = compMove == playerChoice ? "Draw"
+        : compMove == "Rock" && playerChoice == "Paper" ? "You win..."
+            : compMove == "Paper" && playerChoice == "Scissors" ? "You win!"
+                : compMove == "Scissors" && playerChoice == "Rock" ? "You win!"
+                    : compMove == "Paper" && playerChoice == "Rock" ? "You lose..."
+                        : compMove == "Scissors" && playerChoice == "Paper" ? "You lose..."
+                            : "You lose...";
+
+    resultsDisplay.innerHTML = result;
+    console.log(result); 
+}*/
